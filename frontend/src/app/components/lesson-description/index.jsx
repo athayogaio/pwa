c99db6c7fdaf0c35e1799aea6af3aa063e68309f
XeniaGorbunova <GorbunovaKsenia@gmail.com>
2023-02-01 18:03:20 +0300

    fix button names in abonement page

--- frontend/src/app/components/lesson-description/index.jsx
+++ frontend/src/app/components/lesson-description/index.jsx
@@ -17,7 +17,7 @@ import { oneTimeLessonChipContent, scheduleChipContent, weekdays } from '../../u
 
 const LessonDescription = ({
   title, description, price, level, favorite, comments,
-  rate, votes, isVideo, isRegular, startDate, duration, id, payment, schedule,
+  rate, votes, isVideo, isRegular, startDate, duration, id, payment, schedule, isPaid,
 }) => (
   <>
     <Box display="flex" alignItems="start" mb="23px">
@@ -123,13 +123,13 @@ const LessonDescription = ({
     <Box display="flex" justifyContent="flex-end">
       <Button
         component={Link}
-        to={`/abonement/${id}`}
+        to={isPaid ? `/abonement/${id}` : ''}
         variant="contained"
         sx={{
           fontSize: '16px', fontWeight: '500', width: '227px', mb: '20px',
         }}
       >
-        Записаться
+        {isPaid ? 'Записаться' : 'Заниматься'}
       </Button>
     </Box>
   </>
