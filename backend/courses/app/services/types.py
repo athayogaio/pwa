111671691023c92f6a29f7d82d1732e763c9d1a7
chudo <Vse21v@gmail.com>
2023-02-08 11:31:16 +0300

    fix conflict

--- backend/courses/app/services/types.py
+++ backend/courses/app/services/types.py
@@ -41,3 +41,10 @@ class CourseUpdateData(TypedDict):
     course_type: CourseTypes
     level: CourseLevels
     complexity: CourseComplexities
+
+
+class LessonComplaintData(TypedDict):
+    category: str
+    title: str
+    content: str
+    lesson_id: int
